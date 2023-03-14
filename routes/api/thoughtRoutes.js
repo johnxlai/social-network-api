4bbed38e46dd3256570d30b11d7a18ef3162c5fe
John Lai <lai.john.py@gmail.com>
2023-03-14 16:28:54 -0400

    Updat api route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,2 +1,16 @@
 const router = require('express').Router();
+const {} = require('../../controllers/thoughtController');
+
+//grab all thoughts
+//api/thoughts
+router.route('/').get(getAllThoughts);
+
+//api/thoughts/:thoughtId
+router
+  .route('/:thoughtID')
+  .get(getSingleThought)
+  .post(creatNewThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
 module.exports = router;
