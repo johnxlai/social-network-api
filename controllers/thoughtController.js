b451d05009a823849134466cb7412f44f4818a12
John Lai <lai.john.py@gmail.com>
2023-03-15 00:01:24 -0400

    Add thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,9 +2,40 @@ const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
 module.exports = {
-  getAllThoughts(req, res) {},
-  getSingleThought(req, res) {},
+  getAllThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
   createNewThought(req, res) {},
-  updateThought(req, res) {},
-  deleteThought(req, res) {},
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.courseId },
+      { $set: req.body },
+      { new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with this Id' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId }).then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'No thought with this id!' })
+        : res.json(thought)
+    );
+  },
 };
